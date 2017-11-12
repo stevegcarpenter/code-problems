228770df332cbaf38c4781115bef056627515ceb
Steven Carpenter <stevegcarpenter@gmail.com>
2017-11-12 07:11:47 -0800

    implemented sumAndMultiply() function.

--- app.js
+++ app.js
@@ -54,7 +54,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  return [
+    a + b + c,
+    a * b * c,
+    a + ' and ' + b + ' and ' + c + ' sum to ' + (a + b + c) + '.',
+    'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + (a * b * c) + '.',
+    'The product of 4 and 7 and 5 is 140.',
+  ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
